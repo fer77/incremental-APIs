9514ba353b51561c24470c68f642f027594a95a1
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-07-03 13:45:27 -0600

    added notes

--- apis/app/Http/Controllers/LessonsController.php
+++ apis/app/Http/Controllers/LessonsController.php
@@ -9,6 +9,10 @@ class LessonsController extends Controller
 {
     public function index()
     {
+        // 1. "all()" is bad.
+        // 2. no way to attach meta data is bad
+        // 3. linking db structure to API output is bad
+        // 4. no way to signal headers/response codes is bad
         return Lesson::all();
     }
 }
