5f16722897a1d5f63b4d5ddc47b8c30cc1a045d8
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-07-05 10:30:28 -0600

    added responses for lessons that will not exist and lessons that do

--- apis/app/Http/Controllers/LessonsController.php
+++ apis/app/Http/Controllers/LessonsController.php
@@ -13,6 +13,29 @@ class LessonsController extends Controller
         // 2. no way to attach meta data is bad
         // 3. linking db structure to API output is bad
         // 4. no way to signal headers/response codes is bad
-        return Lesson::all();
+        
+        $lessons = Lesson::all();
+        
+        return response()->json([
+            'data' => $lessons->toArray() // nests information within a 'data' array.
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $lesson = Lesson::find($id);
+
+        if (!$lesson)
+        {
+            return response()->json([
+                'error' => [ // will nest errors here to orginize response information.
+                    'message' => 'Lesson does not exist'
+                ]
+            ], 404);
+        }
+        
+        return response()->json([
+            'data' => $lesson->toArray()
+        ], 200);
     }
 }
