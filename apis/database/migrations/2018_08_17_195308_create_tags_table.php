d9463eb601cf060750fc9856672435846b4c0e92
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-08-28 13:45:48 -0600

    created lesson "tag" table

--- apis/database/migrations/2018_08_17_195308_create_tags_table.php
+++ apis/database/migrations/2018_08_17_195308_create_tags_table.php
@@ -14,8 +14,8 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->name('string');
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
