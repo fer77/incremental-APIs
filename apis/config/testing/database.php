bdf370ae5830f71de28673cd574317413fc2dd18
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2018-09-04 14:54:03 -0600

    created a db for unittesting

---
+++ apis/config/testing/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'default' => env('DB_CONNECTION', 'sqlite'),
+
+    'connections' => [
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => ':memory;',
+            'prefix' => '',
+        ]
+    ]
+];
